640eda69567c0bdc85de7e90250ab8eb24b50fe3
Rebecca <rebeccacarpenter316@gmail.com>
2026-09-17 20:55:46 -0400

    feat: added main for testing purposes, added comments for principle violations

--- horrible_code.py
+++ horrible_code.py
@@ -1,6 +1,9 @@
 def calculator(operation):
 
+    # single responsibility violation: one function does multiple different operations
+
     if operation == '+':
+        # DRY code violation: user input lines are repeated multiple times
         num1 = int(input('Enter first number: '))
         num2 = int(input('Enter second number: '))
         return num1 + num2
@@ -18,6 +21,8 @@ def calculator(operation):
     elif operation == '/':
         num1 = int(input('Enter first number: '))
         num2 = int(input('Enter second number: '))
+
+        # KISS violation: using a while loop for division rather than the '/' operator
         quotient = 0
         while num1 >= num2:
             num1 -= num2
@@ -31,3 +36,13 @@ def calculator(operation):
         for i in range(num2):
             result += num1
         return result
+
+# main for testing purposes
+def main():
+    print(calculator('+'))
+    print(calculator('-'))
+    print(calculator('*'))
+    print(calculator('/'))
+
+if __name__ == '__main__':
+    main()
